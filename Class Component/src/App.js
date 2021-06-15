98701fb04e0b6c12605e3aff254f940093116a66
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-06-15 15:59:21 +0600

    Add files via upload

---
+++ Class Component/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card1 from './components/Card1';
+import Card2 from './components/Card2';
+
+
+function App(){
+
+    return (
+         <div>
+
+          <Card1 name="Abu Jakaria Md Numan"/> <br/>
+          <Card2 name = "This is a Class Componenet" />
+
+         </div>
+    ) 
+    }
+    
+
+   
+       
+
+
+export default App;
---
+++ Class Component/src/components/Card1.js
@@ -0,0 +1,10 @@
+function Card1(propos){
+    return  (
+      <div>
+        <h2>  {propos.name} </h2> <br/>
+        <p>I lives in Dhaka Bangladesh</p>
+      </div>
+    )
+}
+
+export default Card1;
